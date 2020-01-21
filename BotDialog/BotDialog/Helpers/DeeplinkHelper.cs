ac0e1049c87d4bb7b3572ed7c0e37741c3d8ae73
mangesh-cyclotrongroup <mangesh@cyclotrongroup.com>
2020-01-21 14:01:53 +0530

    New Bot

---
+++ BotDialog/BotDialog/Helpers/DeeplinkHelper.cs
@@ -0,0 +1,19 @@
+﻿using TeamsHub.SiteRequest.Helper;
+using System.Web;
+
+namespace TeamsHub.SiteRequest.Helpers
+{
+    public static class DeeplinkHelper
+    {
+        public static string GetLeaveBoardDeeplink(string emailId)
+        {
+            return $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.SiteRequest.leaveboard?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl + "?EmailId=" + emailId)}&label=Leave%20Board";
+        }
+
+        public static string PublicHolidaysDeeplink { get; set; } =
+            $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.SiteRequest.holidays?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl + "/first")}&label=Public%20Holidays";
+
+        public static string HelpDeeplink { get; set; } =
+            $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.SiteRequest.help?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl + "/second")}&label=Help";
+    }
+}
