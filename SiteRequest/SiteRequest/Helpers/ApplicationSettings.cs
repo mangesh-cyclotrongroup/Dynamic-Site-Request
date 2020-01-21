d2f73903cd1aca988eae20c1d6739af5efe203b9
mangesh-cyclotrongroup <mangesh@cyclotrongroup.com>
2020-01-21 15:27:29 +0530

    New Changes

---
+++ SiteRequest/SiteRequest/Helpers/ApplicationSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace BotDialog.Helpers
+{
+    public static class ApplicationSettings
+    {
+        public static string BaseUrl { get; set; }
+
+        public static string ConnectionName { get; set; }
+
+        public static string AppId { get; set; }
+
+        static ApplicationSettings()
+        {
+            ConnectionName = ConfigurationManager.AppSettings["ConnectionName"];
+            BaseUrl = ConfigurationManager.AppSettings["BaseUri"];
+
+            AppId = ConfigurationManager.AppSettings["MicrosoftAppId"];
+        }
+    }
+}
